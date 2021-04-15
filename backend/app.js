71f098e2d5e1ae3c088ded7544cb5b57fb16e7bc
kvaibhav <kvaibhav@uci.edu>
2021-04-15 08:07:33 -0700

    Some refactoring added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
+const routes = require("./routes/router")
+
 const app = express();
 
 app.use(bodyparser.json());
@@ -19,30 +21,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/postRefer', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
-  });
-})
-
-app.get('/api/getRefer', (req, res, next) => {
-  const refer = [{
-    id: "fd345",
-    title: "Some title",
-    content: "some content"
-  },
-  {
-    id: "ed345",
-    title: "Some title2",
-    content: "new content"
-  },
-]
-  res.status(200).json({
-    message: "get successfully",
-    posts: refer
-  });
-});
+app.use(routes);
 
 module.exports = app;
