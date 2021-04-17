8b7d5328e24a7781bb05d17218b0bee21e3521a9
Maaz Syed Adeeb <adeeb@uci.edu>
2021-04-17 12:08:34 -0700

    feat: add auto format on commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'src/**/*.{ts,html}': (files) => [
+    // This is because angular-cli just DOES NOT play well with MOST other OSS libs.
+    // Resorting to this to make the files enumerate as required by the CLI
+    `ng lint --fix ${files
+      .map((file) => `--lint-file-patterns ${file}`)
+      .join(' ')}`,
+    `git add ${files.join(' ')}`
+  ]
+};
