601d9f96b01b922a693ac6036b21fc86f6215b4b
Maaz Syed Adeeb <adeebm@uci.edu>
2021-04-22 11:02:24 -0700

    again

--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+import 'globalthis/auto';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-// @ts-ignore
-// eslint-disable-next-line no-var
-var globalThis = require('globalthis')();
 
 declare const require: {
   context(
