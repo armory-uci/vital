05e826e81d454b150957fd39300488086a520ddc
Maaz Syed Adeeb <adeebm@uci.edu>
2021-04-29 13:59:30 -0700

    chore: fix tests

--- src/app/services/http-services/sandbox.service.spec.ts
+++ src/app/services/http-services/sandbox.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { SandboxService } from './sandbox.service';
@@ -6,7 +7,9 @@ describe('SandboxService', () => {
   let service: SandboxService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(SandboxService);
   });
 
