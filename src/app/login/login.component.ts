329c20cc8dc30a4c795c26803d4c7e08c552473f
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2021-05-10 12:57:35 -0700

    Removed actiavted route from login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthProvider } from 'ngx-auth-firebaseui';
 import { ProblemListService } from '../services/utility-services/problem-list.service';
 import { UserInfoService } from '../services/utility-services/user-info.service';
@@ -13,7 +13,6 @@ export class LoginComponent implements OnInit {
   providers = AuthProvider;
 
   constructor(
-    private route: ActivatedRoute,
     private router: Router,
     private userInfoService: UserInfoService,
     private problemListService: ProblemListService
