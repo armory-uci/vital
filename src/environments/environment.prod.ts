fccd8ad2bbdf1113f3ce4ed738534615ac12f39e
Maaz Syed Adeeb <adeebm@uci.edu>
2021-05-10 23:05:00 -0700

    feat: add env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
+import { environment as base } from './environment.base';
+
 export const environment = {
-  production: true,
-  serverUrl: 'http://vital-alb-1836510925.us-east-1.elb.amazonaws.com', // TODO Get this from actual .env file
-  firebaseConfig: {}
+  ...base,
+  // TODO Get this from actual .env file
+  serverUrl: 'http://vital-alb-1836510925.us-east-1.elb.amazonaws.com',
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,12 @@
+import { environment as base } from './environment.base';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-  serverUrl: 'http://localhost:3000',
-  firebaseConfig: {
-    apiKey: 'AIzaSyA_08j-qQznU7TwDNPNK8WrbUkZo5QGtdM',
-    authDomain: 'vital-8262d.firebaseapp.com',
-    projectId: 'vital-8262d',
-    storageBucket: 'vital-8262d.appspot.com',
-    messagingSenderId: '253296918538',
-    appId: '1:253296918538:web:61ee5a41302d2d72ac9ff3',
-    measurementId: 'G-MF7ES3S7H6'
-  }
+  ...base,
+  production: false
 };
 
 /*
