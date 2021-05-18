17e82bad20a894586f3d2aacf0e53bf3a321665d
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2021-05-18 02:33:28 -0700

    VIT-12 Styling and more work

--- src/app/home/problem-list/user-profile/user-profile.component.ts
+++ src/app/home/problem-list/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IUserInfo } from 'src/app/login/login.model';
+import { Tile } from './user-profile.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  @Input() userInfo: IUserInfo;
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  resetProgress() {}
 }
