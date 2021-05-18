600fa1eb09e150290ec59305923126d32f4b8ede
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2021-05-18 09:41:17 -0700

    User Profile Design

--- src/app/home/problem-list/user-profile/user-profile.component.ts
+++ src/app/home/problem-list/user-profile/user-profile.component.ts
@@ -9,13 +9,6 @@ import { Tile } from './user-profile.model';
 })
 export class UserProfileComponent implements OnInit {
   @Input() userInfo: IUserInfo;
-  tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' }
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
